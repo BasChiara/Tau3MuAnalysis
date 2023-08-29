6b59c531fe28fee8078c280678a39c9f0015197f
Chiara Basile <basilechiara@libero.it>
2023-08-29 17:16:38 +0200

    to run RecoLevelAnalysis on 2022

---
+++ PreliminaryStudies/scripts/to_submit.sh
@@ -0,0 +1,4 @@
+# run ./bin/RecoLevelAnalysis on data
+
+## 2022 era E ##
+python scripts/submit_batch.py -N -1 -n 1 -F 1107 -p job_report --eos /eos/user/c/cbasile/Tau3MuRun3/CMSSW_12_4_11/src/Tau3MuAnalysis/condor_data/ data/ParkingDoubleMuonLowMass0_2022E.txt
