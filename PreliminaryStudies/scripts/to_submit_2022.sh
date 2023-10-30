d4c173b9e3cb4b91618c81417b4f35af3ab59940
Chiara Basile <basilechiara@libero.it>
2023-10-30 11:23:46 +0100

    [+] 2022 era G

--- PreliminaryStudies/scripts/to_submit_2022.sh
+++ PreliminaryStudies/scripts/to_submit_2022.sh
@@ -1,22 +1,32 @@
 # run ./bin/RecoLevelAnalysis on data
-
+make clean
+make reco
 ## 2022 era E ##
-python scripts/submit_batch.py -N -1 -n 1 -F 1107 -p job_report --eos /eos/user/c/cbasile/Tau3MuRun3/CMSSW_12_4_11/src/Tau3MuAnalysis/condor_data/ data/ParkingDoubleMuonLowMass0_2022E.txt
-python scripts/submit_batch.py -N -1 -n 1 -F 1105 -p job_report --eos /eos/user/c/cbasile/Tau3MuRun3/CMSSW_12_4_11/src/Tau3MuAnalysis/condor_data/ data/ParkingDoubleMuonLowMass1_2022E.txt
-python scripts/submit_batch.py -N -1 -n 1 -F 1107 -p job_report --eos /eos/user/c/cbasile/Tau3MuRun3/CMSSW_12_4_11/src/Tau3MuAnalysis/condor_data/ data/ParkingDoubleMuonLowMass2_2022E.txt
-python scripts/submit_batch.py -N -1 -n 1 -F 1105 -p job_report --eos /eos/user/c/cbasile/Tau3MuRun3/CMSSW_12_4_11/src/Tau3MuAnalysis/condor_data/ data/ParkingDoubleMuonLowMass3_2022E.txt
-python scripts/submit_batch.py -N -1 -n 1 -F 1104 -p job_report --eos /eos/user/c/cbasile/Tau3MuRun3/CMSSW_12_4_11/src/Tau3MuAnalysis/condor_data/ data/ParkingDoubleMuonLowMass4_2022E.txt
-python scripts/submit_batch.py -N -1 -n 1 -F 1104 -p job_report --eos /eos/user/c/cbasile/Tau3MuRun3/CMSSW_12_4_11/src/Tau3MuAnalysis/condor_data/ data/ParkingDoubleMuonLowMass5_2022E.txt
-python scripts/submit_batch.py -N -1 -n 1 -F 1108 -p job_report --eos /eos/user/c/cbasile/Tau3MuRun3/CMSSW_12_4_11/src/Tau3MuAnalysis/condor_data/ data/ParkingDoubleMuonLowMass6_2022E.txt
-python scripts/submit_batch.py -N -1 -n 1 -F 1105 -p job_report --eos /eos/user/c/cbasile/Tau3MuRun3/CMSSW_12_4_11/src/Tau3MuAnalysis/condor_data/ data/ParkingDoubleMuonLowMass7_2022E.txt
+python scripts/submit_batch.py -N -1 -n 1 -F 1107 -p job_report --eos /eos/user/c/cbasile/Tau3MuRun3/CMSSW_12_4_11/src/Tau3MuAnalysis/condor_data/Run3_2022/ data/ParkingDoubleMuonLowMass0_2022E.txt
+python scripts/submit_batch.py -N -1 -n 1 -F 1105 -p job_report --eos /eos/user/c/cbasile/Tau3MuRun3/CMSSW_12_4_11/src/Tau3MuAnalysis/condor_data/Run3_2022/ data/ParkingDoubleMuonLowMass1_2022E.txt
+#python scripts/submit_batch.py -N -1 -n 1 -F 1107 -p job_report --eos /eos/user/c/cbasile/Tau3MuRun3/CMSSW_12_4_11/src/Tau3MuAnalysis/condor_data/Run3_2022/ data/ParkingDoubleMuonLowMass2_2022E.txt
+python scripts/submit_batch.py -N -1 -n 1 -F 1105 -p job_report --eos /eos/user/c/cbasile/Tau3MuRun3/CMSSW_12_4_11/src/Tau3MuAnalysis/condor_data/Run3_2022/ data/ParkingDoubleMuonLowMass3_2022E.txt
+python scripts/submit_batch.py -N -1 -n 1 -F 1104 -p job_report --eos /eos/user/c/cbasile/Tau3MuRun3/CMSSW_12_4_11/src/Tau3MuAnalysis/condor_data/Run3_2022/ data/ParkingDoubleMuonLowMass4_2022E.txt
+python scripts/submit_batch.py -N -1 -n 1 -F 1104 -p job_report --eos /eos/user/c/cbasile/Tau3MuRun3/CMSSW_12_4_11/src/Tau3MuAnalysis/condor_data/Run3_2022/ data/ParkingDoubleMuonLowMass5_2022E.txt
+python scripts/submit_batch.py -N -1 -n 1 -F 1108 -p job_report --eos /eos/user/c/cbasile/Tau3MuRun3/CMSSW_12_4_11/src/Tau3MuAnalysis/condor_data/Run3_2022/ data/ParkingDoubleMuonLowMass6_2022E.txt
+python scripts/submit_batch.py -N -1 -n 1 -F 1105 -p job_report --eos /eos/user/c/cbasile/Tau3MuRun3/CMSSW_12_4_11/src/Tau3MuAnalysis/condor_data/Run3_2022/ data/ParkingDoubleMuonLowMass7_2022E.txt
 
 ## 2022 era F ##
-python scripts/submit_batch.py -N -1 -n 1 -F 2596 -p job_report --eos /eos/user/c/cbasile/Tau3MuRun3/CMSSW_12_4_11/src/Tau3MuAnalysis/condor_data/ data/ParkingDoubleMuonLowMass0_2022F.txt
-python scripts/submit_batch.py -N -1 -n 1 -F 2593 -p job_report --eos /eos/user/c/cbasile/Tau3MuRun3/CMSSW_12_4_11/src/Tau3MuAnalysis/condor_data/ data/ParkingDoubleMuonLowMass1_2022F.txt
-python scripts/submit_batch.py -N -1 -n 1 -F 2596 -p job_report --eos /eos/user/c/cbasile/Tau3MuRun3/CMSSW_12_4_11/src/Tau3MuAnalysis/condor_data/ data/ParkingDoubleMuonLowMass2_2022F.txt
-python scripts/submit_batch.py -N -1 -n 1 -F 2594 -p job_report --eos /eos/user/c/cbasile/Tau3MuRun3/CMSSW_12_4_11/src/Tau3MuAnalysis/condor_data/ data/ParkingDoubleMuonLowMass3_2022F.txt
-python scripts/submit_batch.py -N -1 -n 1 -F 2592 -p job_report --eos /eos/user/c/cbasile/Tau3MuRun3/CMSSW_12_4_11/src/Tau3MuAnalysis/condor_data/ data/ParkingDoubleMuonLowMass4_2022F.txt
-python scripts/submit_batch.py -N -1 -n 1 -F 2592 -p job_report --eos /eos/user/c/cbasile/Tau3MuRun3/CMSSW_12_4_11/src/Tau3MuAnalysis/condor_data/ data/ParkingDoubleMuonLowMass5_2022F.txt
-python scripts/submit_batch.py -N -1 -n 1 -F 2589 -p job_report --eos /eos/user/c/cbasile/Tau3MuRun3/CMSSW_12_4_11/src/Tau3MuAnalysis/condor_data/ data/ParkingDoubleMuonLowMass6_2022F.txt
-python scripts/submit_batch.py -N -1 -n 1 -F 2597 -p job_report --eos /eos/user/c/cbasile/Tau3MuRun3/CMSSW_12_4_11/src/Tau3MuAnalysis/condor_data/ data/ParkingDoubleMuonLowMass7_2022F.txt
+#python scripts/submit_batch.py -N -1 -n 1 -F 2596 -p job_report --eos /eos/user/c/cbasile/Tau3MuRun3/CMSSW_12_4_11/src/Tau3MuAnalysis/condor_data/Run3_2022/ data/ParkingDoubleMuonLowMass0_2022F.txt
+python scripts/submit_batch.py -N -1 -n 1 -F 2593 -p job_report --eos /eos/user/c/cbasile/Tau3MuRun3/CMSSW_12_4_11/src/Tau3MuAnalysis/condor_data/Run3_2022/ data/ParkingDoubleMuonLowMass1_2022F.txt
+python scripts/submit_batch.py -N -1 -n 1 -F 2596 -p job_report --eos /eos/user/c/cbasile/Tau3MuRun3/CMSSW_12_4_11/src/Tau3MuAnalysis/condor_data/Run3_2022/ data/ParkingDoubleMuonLowMass2_2022F.txt
+python scripts/submit_batch.py -N -1 -n 1 -F 2594 -p job_report --eos /eos/user/c/cbasile/Tau3MuRun3/CMSSW_12_4_11/src/Tau3MuAnalysis/condor_data/Run3_2022/ data/ParkingDoubleMuonLowMass3_2022F.txt
+python scripts/submit_batch.py -N -1 -n 1 -F 2592 -p job_report --eos /eos/user/c/cbasile/Tau3MuRun3/CMSSW_12_4_11/src/Tau3MuAnalysis/condor_data/Run3_2022/ data/ParkingDoubleMuonLowMass4_2022F.txt
+python scripts/submit_batch.py -N -1 -n 1 -F 2592 -p job_report --eos /eos/user/c/cbasile/Tau3MuRun3/CMSSW_12_4_11/src/Tau3MuAnalysis/condor_data/Run3_2022/ data/ParkingDoubleMuonLowMass5_2022F.txt
+python scripts/submit_batch.py -N -1 -n 1 -F 2589 -p job_report --eos /eos/user/c/cbasile/Tau3MuRun3/CMSSW_12_4_11/src/Tau3MuAnalysis/condor_data/Run3_2022/ data/ParkingDoubleMuonLowMass6_2022F.txt
+python scripts/submit_batch.py -N -1 -n 1 -F 2597 -p job_report --eos /eos/user/c/cbasile/Tau3MuRun3/CMSSW_12_4_11/src/Tau3MuAnalysis/condor_data/Run3_2022/ data/ParkingDoubleMuonLowMass7_2022F.txt
 
+## 2022 era G ##
+python scripts/submit_batch.py -N -1 -n 1 -F 420 -p job_report --eos /eos/user/c/cbasile/Tau3MuRun3/CMSSW_12_4_11/src/Tau3MuAnalysis/condor_data/Run3_2022/ data/ParkingDoubleMuonLowMass0_2022G.txt
+python scripts/submit_batch.py -N -1 -n 1 -F 419 -p job_report --eos /eos/user/c/cbasile/Tau3MuRun3/CMSSW_12_4_11/src/Tau3MuAnalysis/condor_data/Run3_2022/ data/ParkingDoubleMuonLowMass1_2022G.txt
+python scripts/submit_batch.py -N -1 -n 1 -F 420 -p job_report --eos /eos/user/c/cbasile/Tau3MuRun3/CMSSW_12_4_11/src/Tau3MuAnalysis/condor_data/Run3_2022/ data/ParkingDoubleMuonLowMass2_2022G.txt
+python scripts/submit_batch.py -N -1 -n 1 -F 422 -p job_report --eos /eos/user/c/cbasile/Tau3MuRun3/CMSSW_12_4_11/src/Tau3MuAnalysis/condor_data/Run3_2022/ data/ParkingDoubleMuonLowMass3_2022G.txt
+python scripts/submit_batch.py -N -1 -n 1 -F 419 -p job_report --eos /eos/user/c/cbasile/Tau3MuRun3/CMSSW_12_4_11/src/Tau3MuAnalysis/condor_data/Run3_2022/ data/ParkingDoubleMuonLowMass4_2022G.txt
+python scripts/submit_batch.py -N -1 -n 1 -F 420 -p job_report --eos /eos/user/c/cbasile/Tau3MuRun3/CMSSW_12_4_11/src/Tau3MuAnalysis/condor_data/Run3_2022/ data/ParkingDoubleMuonLowMass5_2022G.txt
+python scripts/submit_batch.py -N -1 -n 1 -F 420 -p job_report --eos /eos/user/c/cbasile/Tau3MuRun3/CMSSW_12_4_11/src/Tau3MuAnalysis/condor_data/Run3_2022/ data/ParkingDoubleMuonLowMass6_2022G.txt
+python scripts/submit_batch.py -N -1 -n 1 -F 419 -p job_report --eos /eos/user/c/cbasile/Tau3MuRun3/CMSSW_12_4_11/src/Tau3MuAnalysis/condor_data/Run3_2022/ data/ParkingDoubleMuonLowMass7_2022G.txt
