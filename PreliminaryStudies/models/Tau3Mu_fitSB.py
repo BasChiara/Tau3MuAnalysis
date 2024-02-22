41e28014ec8a3aecdbe838ebd5219c4d04fa9601
Chiara Basile <basilechiara@libero.it>
2024-02-22 17:36:27 +0100

    prepare inputs for combine

--- PreliminaryStudies/models/Tau3Mu_fitSB.py
+++ PreliminaryStudies/models/Tau3Mu_fitSB.py
@@ -15,12 +15,17 @@ parser = argparse.ArgumentParser()
 parser.add_argument('--plot_outdir',default= '/eos/user/c/cbasile/www/Tau3Mu_Run3/SigBkg_models/Tau3Mu_massFit/reMini', help=' output directory for plots')
 parser.add_argument('--tag',        default= 'emulateRun2', help='tag to the training')
 parser.add_argument('--debug',      action = 'store_true' ,help='set it to have useful printout')
+parser.add_argument('-u','--unblind',action = 'store_true' ,help='set it to run UN-blind')
+parser.add_argument('--save_ws',    action = 'store_true' ,help='set it to save the workspace for combine')
 parser.add_argument('--category',   default = 'noCat')
+parser.add_argument('-y','--year',  default = '22')
 parser.add_argument('--bdt_cut',    type= float, default = 0.990)
 
 args = parser.parse_args()
-tag = 'bdt%d_%s'%(args.bdt_cut*1000, args.category) + ('_' + args.tag ) if not (args.tag is None) else ''
 
+
+tag = 'bdt%d_%s%s'%(args.bdt_cut*1000, args.category, args.year) + ('_' + args.tag ) if not (args.tag is None) else ''
+process_name = 'WTau3Mu_%s%s'%(args.category, args.year)
 
 ROOT.gROOT.SetBatch(True)
 ROOT.gStyle.SetOptStat(True)
@@ -29,12 +34,12 @@ ROOT.TH1.SetDefaultSumw2()
 # **** USEFUL CONSTANT VARIABLES *** #
 mass_window = 0.060 # GeV
 tau_mass = 1.777 # GeV
-fit_range_lo  , fit_range_hi   = 1.73, 1.82 # GeV
+fit_range_lo  , fit_range_hi   = 1.70, 1.85 # GeV
 mass_window_lo, mass_window_hi = 1.60, 2.00 # GeV #tau_mass-mass_window, tau_mass+mass_window
 
 nbins = 40 # needed just for plotting, fits are all unbinned
 
-blinded = True # don't show (nor fit!) data in the signal mass window
+runblind = not args.unblind # don't show (nor fit!) data in the signal mass window
 blind_region_lo, blind_region_hi = 1.72, 1.84
 # phi
 phi_mass = 1.020 #GeV
@@ -43,8 +48,8 @@ omega_mass = 0.783 #GeV
 
 
 input_tree_name = 'tree_w_BDT'
-mc_file = '/eos/user/c/cbasile/Tau3MuRun3/data/mva_data/XGBout_signal_reMini22_23_kFold_2024Feb02.root'
-data_file = '/eos/user/c/cbasile/Tau3MuRun3/data/mva_data/XGBout_data_reMini22_23_kFold_2024Feb02_blind.root'
+mc_file = '/eos/user/c/cbasile/Tau3MuRun3/data/mva_data/XGBout_signal_reMini2022_kFold_2024Feb22.root'
+data_file = '/eos/user/c/cbasile/Tau3MuRun3/data/mva_data/XGBout_data_reMini2022_kFold_2024Feb22_open.root'
 
 
 # ** RooFit Variables
@@ -82,8 +87,8 @@ thevars.add(run)
 sgn_selection = 'bdt_score > %.4f'%args.bdt_cut
 phi_veto = '''fabs(tau_mu12_fitM- {mass:.3f})> {window:.3f} & fabs(tau_mu23_fitM - {mass:.3f})> {window:.3f} & fabs(tau_mu13_fitM -  {mass:.3f})>{window:.3f}'''.format(mass =phi_mass , window = phi_window/2. )
 cat_selection = ''
-if args.category == 'A'  : cat_selection = ' & tau_fit_mass_err/tau_fit_mass < 0.007'
-if args.category == 'B'  : cat_selection = ' & tau_fit_mass_err/tau_fit_mass > 0.007 & tau_fit_mass_err/tau_fit_mass < 0.012'
+if args.category == 'A'  : cat_selection = ' & tau_fit_mass_err/tau_fit_mass <= 0.007'
+if args.category == 'B'  : cat_selection = ' & tau_fit_mass_err/tau_fit_mass > 0.007 & tau_fit_mass_err/tau_fit_mass <= 0.012'
 if args.category == 'C'  : cat_selection = ' & tau_fit_mass_err/tau_fit_mass > 0.012'
 if args.category == 'AB' : cat_selection = ' & tau_fit_mass_err/tau_fit_mass < 0.012'
 base_selection = phi_veto + '&' + cat_selection + ' & run < 362800'
@@ -93,7 +98,7 @@ mc_tree = ROOT.TChain(input_tree_name)
 mc_tree.AddFile(mc_file)
 N_mc = mc_tree.GetEntries(base_selection)
 
-fullmc = ROOT.RooDataSet('mc_WTau3Mu', 'mc_WTau3Mu', mc_tree, thevars, sgn_selection, "weight")
+fullmc = ROOT.RooDataSet('mc_%s'%process_name, 'mc_%s'%process_name, mc_tree, thevars, sgn_selection, "weight")
 fullmc.Print()
 print('entries = %.2f'%fullmc.sumEntries() )
 print('weight  = %e'%fullmc.weight() )
@@ -102,22 +107,23 @@ sig_efficiency = fullmc.sumEntries()/N_mc/fullmc.weight()
 # signal PDF
 Mtau   = ROOT.RooRealVar('Mtau' , 'Mtau' , tau_mass, -1.7, 1.9)
 Mtau.setConstant(True)
-dMtau  = ROOT.RooRealVar('dM', 'dM', 0, -1., 1.)
+dMtau  = ROOT.RooRealVar('dM', 'dM', 0, -0.1, 0.1)
 mean   = ROOT.RooFormulaVar('mean','mean', '(@0+@1)', ROOT.RooArgList(Mtau,dMtau) )
-width  = ROOT.RooRealVar('width',  'width', 0.01,    0.001, 0.10)
-gaus   = ROOT.RooGaussian('model_WTau3Mu', 'model_WTau3Mu', mass, mean, width)
+width  = ROOT.RooRealVar('width',  'width', 0.01,    0.005, 0.05)
 
-f = ROOT.RooRealVar("f", "", 0., 1.)
-nsig = ROOT.RooRealVar('Ns', 'model_WTau3Mu_norm', fullmc.sumEntries(), 0., 3*fullmc.sumEntries())
-signal_model = ROOT.RooAddPdf("ext_model_WTau3Mu", "model_WTau3Mu", ROOT.RooArgList(gaus), nsig )
+nsig   = ROOT.RooRealVar('model_sig_%s_norm'%process_name, 'model_sig_%s_norm'%process_name, fullmc.sumEntries(), 0., 3*fullmc.sumEntries())
+gaus   = ROOT.RooGaussian('model_sig_%s'%process_name, 'model_sig_%s'%process_name, mass, mean, width)
+
+signal_model = ROOT.RooAddPdf('ext_model_sig_%s'%process_name, 'ext_model_sig_%s'%process_name, ROOT.RooArgList(gaus), nsig )
 
 # signal fit
 results_gaus = signal_model.fitTo(
     fullmc, 
-    ROOT.RooFit.Range('fit_range'), 
+    ROOT.RooFit.Range('sig_range'), 
     ROOT.RooFit.Save(),
     ROOT.RooFit.Extended(ROOT.kTRUE),
     ROOT.RooFit.SumW2Error(True),
+    ROOT.RooFit.PrintLevel(-1),
 )
 
 # * draw & save
@@ -127,25 +133,23 @@ frame.SetTitle('#tau -> 3 #mu signal - CAT %s BDTscore > %.4f'%(args.category, a
 fullmc.plotOn(
     frame, 
     ROOT.RooFit.Binning(nbins), 
-    #ROOT.RooFit.DrawOption('B'), 
-    #ROOT.RooFit.DataError(ROOT.RooAbsData.None), 
     ROOT.RooFit.XErrorSize(0), 
     ROOT.RooFit.LineWidth(2),
     ROOT.RooFit.FillColor(ROOT.kRed),
-    #ROOT.RooFit.FillStyle(3003),                
+    DataError="SumW2",
 )
-
 signal_model.plotOn(
     frame, 
     ROOT.RooFit.LineColor(ROOT.kRed),
-    ROOT.RooFit.Range('sig_range'),
-    ROOT.RooFit.NormRange('fit_range')
+    ROOT.RooFit.Range('full_range'),
+    ROOT.RooFit.NormRange('sig_range'),
+    ROOT.RooFit.MoveToBack()
 )
 print('signal chi2 %.2f'%(frame.chiSquare()))
 signal_model.paramOn(
     frame,
     ROOT.RooFit.Title("Signal Fit parameters:"),
-    ROOT.RooFit.Layout(0.6, 0.95, 0.9),
+    ROOT.RooFit.Layout(0.6, 0.75, 0.9),
     ROOT.RooFit.Format("NEU", ROOT.RooFit.AutoPrecision(1)),
 )
 frame.getAttText().SetTextSize(0.03)
@@ -159,6 +163,16 @@ c.SetLogy(1)
 c.SaveAs('%s/signal_mass_Log_%s.png'%(args.plot_outdir, tag)) 
 c.SaveAs('%s/signal_mass_Log_%s.pdf'%(args.plot_outdir, tag)) 
 
+# -- plot pulls
+h_pullMC = frame.pullHist()
+frame_pull = mass.frame(Title = '[pull] #tau -> 3 #mu signal - CAT %s BDTscore > %.4f'%(args.category, args.bdt_cut))
+frame_pull.addPlotable(h_pullMC, 'P')
+cp = ROOT.TCanvas("cp", "cp", 800, 800)
+ROOT.gPad.SetMargin(0.15,0.15,0.15,0.15)
+frame_pull.Draw()
+cp.SaveAs('%s/pull_signal_mass_%s.png'%(args.plot_outdir, tag)) 
+cp.SaveAs('%s/pull_signal_mass_%s.pdf'%(args.plot_outdir, tag)) 
+
 #### DATA ####
 base_selection += ' & ((tau_fit_mass < %.3f )|| (tau_fit_mass > %.3f))'%(blind_region_lo, blind_region_hi)
 
@@ -166,7 +180,7 @@ data_tree = ROOT.TChain(input_tree_name)
 data_tree.AddFile(data_file)
 N_data = data_tree.GetEntries(base_selection) 
 
-if blinded:
+if runblind:
     print('BLIND')
     # cut for blinding
     blinder  = ROOT.RooFormulaVar('blinder', 'blinder',  ' ((tau_fit_mass < %.3f )|| (tau_fit_mass > %.3f)) & (%s)'%(blind_region_lo, blind_region_hi, sgn_selection) , ROOT.RooArgList(thevars))
@@ -178,7 +192,7 @@ datatofit.Print()
 bkg_efficiency = datatofit.sumEntries()/N_data
 
 frame_b = mass.frame()
-frame_b.SetTitle('#tau -> 3 #mu signal - CAT %s BDTscore > %.3f'%(args.category, args.bdt_cut))
+frame_b.SetTitle('#tau -> 3 #mu signal+bkg - CAT %s BDTscore > %.3f'%(args.category, args.bdt_cut))
 
 datatofit.plotOn(
     frame_b, 
@@ -187,17 +201,15 @@ datatofit.plotOn(
 )
 
 # background PDF
-slope = ROOT.RooRealVar('slope', 'slope', -1.0, -50.0, 0.001)
-expo  = ROOT.RooExponential('model_bkg_WTau3Mu', 'model_bkg_WTau3Mu', mass, slope)
+slope = ROOT.RooRealVar('slope', 'slope', -1.0, -10.0, 10.0)
+expo  = ROOT.RooExponential('model_bkg_%s'%process_name, 'model_bkg_%s'%process_name, mass, slope)
 
 # number of background events
-#nbkg = ROOT.RooRealVar('model_bkg_WTau3Mu_norm', 'model_bkg_WTau3Mu_norm', datatofit.numEntries(), 0., 3*datatofit.numEntries())
-nbkg = ROOT.RooRealVar('Nb', 'model_bkg_WTau3Mu_norm', datatofit.numEntries(), 0., 3*datatofit.numEntries())
-#ext_bkg_model = ROOT.RooExtendPdf("ext_model_bkg_WTau3Mu", "extended background pdf", expo, nbkg)
+nbkg = ROOT.RooRealVar('model_bkg_%s_norm'%process_name, 'model_bkg_%s_norm'%process_name, datatofit.numEntries(), 0., 3*datatofit.numEntries())
 ext_bkg_model = ROOT.RooAddPdf("toy_add_model_bkg_WTau3Mu", "add background pdf", ROOT.RooArgList(expo),  ROOT.RooArgList(nbkg))
 
 # fit background with exponential model
-results_expo = ext_bkg_model.fitTo(datatofit, ROOT.RooFit.Range('left_SB,right_SB'), ROOT.RooFit.Save())
+results_expo = ext_bkg_model.fitTo(datatofit, ROOT.RooFit.Range('left_SB,right_SB'), ROOT.RooFit.Save(),ROOT.RooFit.PrintLevel(-1))
 ext_bkg_model.plotOn(
     frame_b, 
     ROOT.RooFit.LineColor(ROOT.kBlue),
@@ -248,24 +260,72 @@ print(' -- BaseSelection %s'%base_selection)
 print(' == S efficiency %.2f '%sig_efficiency)
 print(' == B efficiency %.2e '%bkg_efficiency)
 
-exit(-1)
+if not args.save_ws : exit(-1)
 # ----------------------------------------------------------------------------------------------------
 #### SAVE MODEL TO A WORKSPACE ####
+wspace_filename = "input_combine/wspace_%s_%s.root"%(process_name,tag)
+wspace_name     = "WTau3Mu_w"
+# fix the signal shape
 dMtau.setConstant(True)
-width1.setConstant(True)
-width2.setConstant(True)
-f.setConstant(True)
+#width.setConstant(True)
+
+# save observed data // bkg-only Asimov with name 'dat_obs'
 fulldata = ROOT.RooDataSet('full_data', 'full_data', data_tree, thevars, sgn_selection)
-data     = ROOT.RooDataSet('data_obs','data_obs', fulldata, ROOT.RooArgSet(mass))
+if runblind:
+    # GenerateAsimovData() generates binned data following the binning of the observables
+    mass.setBins(nbins)
+    data = ROOT.RooStats.AsymptoticCalculator.GenerateAsimovData(ext_bkg_model, ROOT.RooArgSet(mass) )
+    data.SetName('data_obs') 
+else :
+    data     = ROOT.RooDataSet('data_obs','data_obs', fulldata, ROOT.RooArgSet(mass))
 data.Print()
-ws = ROOT.RooWorkspace("workspace_SB", "workspace_SB")
+
+ws = ROOT.RooWorkspace(wspace_name, wspace_name)
 getattr(ws, 'import')(data)
-getattr(ws, 'import')(signal_model)
+getattr(ws, 'import')(gaus)
 getattr(ws, 'import')(nsig)
 getattr(ws, 'import')(expo)
 getattr(ws, 'import')(nbkg)
-f_out_name = "workspace_2022SB_%s.root"%tag
-f_out = ROOT.TFile(f_out_name, "RECREATE")
 ws.Print()
-ws.Write()
-f_out.Close()
+ws.writeToFile(wspace_filename)
+
+#### WRITE THE DATACARD ####
+datacard_name = "input_combine/datacard_%s_%s.txt"%(process_name,tag)
+# dump the text datacard
+with open(datacard_name, 'w') as card:
+   card.write(
+'''
+imax 1 number of channels
+jmax * number of background sources
+kmax * number of nuisance parameters
+--------------------------------------------------------------------------------
+shapes bkg         {proc}       {ws_file} {ws_name}:{bkg_model}
+shapes sig         {proc}       {ws_file} {ws_name}:{sig_model}
+shapes data_obs    {proc}       {ws_file} {ws_name}:data_obs
+--------------------------------------------------------------------------------
+bin                {proc}
+observation        {obs:d}
+--------------------------------------------------------------------------------
+bin                                     {proc}              {proc}
+process                                 sig                 bkg
+process                                 0                   1
+rate                                    {signal:.4f}        {bkg:.4f}
+--------------------------------------------------------------------------------
+bkg_norm rateParam                   {proc}      bkg     1. 
+slope    param  {slopeval:.4f} {slopeerr:.4f}
+'''.format(
+         proc     = process_name, 
+         ws_file  = wspace_filename, 
+         ws_name  = wspace_name, 
+         bkg_model= expo.GetName(),
+         sig_model= gaus.GetName(),
+         obs      = fulldata.numEntries() if runblind==False else -1, # number of observed events
+         signal   = fullmc.sumEntries(), # number of EXPECTED signal events, INCLUDES the a priori normalisation. Combine fit results will be in terms of signal strength relative to this inistial normalisation
+         bkg      = nbkg.getVal(), # number of expected background events **over the full mass range** using the exponential funciton fitted in the sidebands 
+         slopeval = slope.getVal(),
+         slopeerr = slope.getError()
+         )
+)
+
+
+
