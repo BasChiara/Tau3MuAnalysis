9c0a84eb85a5d1df30cccaf3e4b8f6e570a26aae
Chiara Basile <basilechiara@libero.it>
2024-05-24 17:56:56 +0200

    merge scripts

---
+++ scripts/to_reco_WTau3Mu.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/bash
+# 2022
+./bin/Analyzer_app -i data/mc_reMini2022/MonteCarlo_Tau3Mu_2022preEE.txt -o outRoot/ -d MC -y 2022preEE -a Tau3Mu -p Tau3Mu
+./bin/Analyzer_app -i data/mc_reMini2022/MonteCarlo_Tau3Mu_2022EE.txt -o outRoot/ -d MC -y 2022EE -a Tau3Mu -p Tau3Mu
+# 2023
