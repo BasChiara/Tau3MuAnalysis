e9e64bf15de756cd4add1125cf5316eb0f461e72
Chiara Basile <basilechiara@libero.it>
2026-02-17 16:15:06 +0100

    test for HF

---
+++ test/dimuon-veto/HF-test/commands.sh
@@ -0,0 +1,18 @@
+# VETO BY CATEGORY AND YEAR
+# -- phi->mumu
+#python3 dimuon_fits.py --plot_outdir ./plots-v2/ --debug --category A --year 2022 --bdt_cut 0.7 --mu_pair 1 --resonance phi
+python3 dimuon_fits.py --plot_outdir ./plots-v2/ --debug --category B --year 2022 --bdt_cut 0.7 --mu_pair 1 --resonance phi
+python3 dimuon_fits.py --plot_outdir ./plots-v2/ --debug --category C --year 2022 --bdt_cut 0.7 --mu_pair 1 --resonance phi
+# --
+python3 dimuon_fits.py --plot_outdir ./plots-v2/ --debug --category A --year 2023 --bdt_cut 0.7 --mu_pair 1 --resonance phi
+python3 dimuon_fits.py --plot_outdir ./plots-v2/ --debug --category B --year 2023 --bdt_cut 0.7 --mu_pair 1 --resonance phi
+python3 dimuon_fits.py --plot_outdir ./plots-v2/ --debug --category C --year 2023 --bdt_cut 0.7 --mu_pair 1 --resonance phi
+
+# -- omega->mumu
+python3 dimuon_fits.py --plot_outdir ./plots-v2/ --debug --category A --year 2022 --bdt_cut 0.7 --mu_pair 1 --resonance omega
+python3 dimuon_fits.py --plot_outdir ./plots-v2/ --debug --category B --year 2022 --bdt_cut 0.7 --mu_pair 1 --resonance omega
+python3 dimuon_fits.py --plot_outdir ./plots-v2/ --debug --category C --year 2022 --bdt_cut 0.7 --mu_pair 1 --resonance omega
+# --
+python3 dimuon_fits.py --plot_outdir ./plots-v2/ --debug --category A --year 2023 --bdt_cut 0.7 --mu_pair 1 --resonance omega
+python3 dimuon_fits.py --plot_outdir ./plots-v2/ --debug --category B --year 2023 --bdt_cut 0.7 --mu_pair 1 --resonance omega
+python3 dimuon_fits.py --plot_outdir ./plots-v2/ --debug --category C --year 2023 --bdt_cut 0.7 --mu_pair 1 --resonance omega
